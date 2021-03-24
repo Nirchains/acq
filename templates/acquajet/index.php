9286f59c06eef8cb13693b2d2d0ee99ecd469dd4
Pedro Fernández <nirchains@gmail.com>
2021-03-24 10:38:36 +0100

    Eliminar tags

--- templates/acquajet/index.php
+++ templates/acquajet/index.php
@@ -447,43 +447,7 @@ if ($this->params->get('smooth_scroll_version') == '0') {
             $doc->addScriptDeclaration('window.addEvent("domready",function(){Element.prototype.hide=function(){}});');
         }
         ?>
-
-        <script type="text/javascript">
-	/* <![CDATA[ */
-		var google_conversion_id = 1024651226;var google_custom_params = window.google_tag_params;var google_remarketing_only = true;/* ]]> */
-		</script>
-		<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
-		<noscript><div style="display:inline;"><img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/1024651226/?value=0&amp;guid=ON&amp;script=0"/></div></noscript>
-
-		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-12408332-1', 'auto');  ga('send', 'pageview');
-		</script>
-
-		<script>
-		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-		document,'script','https://connect.facebook.net/en_US/fbevents.js');
-
-		fbq('init', '438228106364588');
-		fbq('track', "PageView");</script>
-		<noscript><img height="1" width="1" alt="" style="display:none"
-		src="https://www.facebook.com/tr?id=438228106364588&ev=PageView&noscript=1"
-		/></noscript>
-
-		<!-- Google Tag Manager -->
-		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5D6QK8"
-		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-5D6QK8');</script>
-		<!-- End Google Tag Manager -->
-
-
-    
+   
         <jdoc:include type="modules" name="debug" />
         <!-- Preloader -->
         <jdoc:include type="modules" name="helixpreloader" />
